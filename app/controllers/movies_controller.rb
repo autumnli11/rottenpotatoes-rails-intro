37b34acb5a1d8a87e854e6e09b1f2079d42021c7
“Autumn <qiushili@berkeley.edu>
2019-10-13 16:23:39 -0700

    heroku

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,6 +12,7 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
+    @all_ratings = Movie.get_all_ratings
     if params[:sort_by] == "title"
       @movies = @movies.order(:title)
       @title_header = 'hilite'
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ActiveRecord::Base
+
+    @@all_ratings = self.find(:first, :group => "rating").map(&:rating)
+    def get_all_ratings
+        @@all_ratings
+    end
 end
