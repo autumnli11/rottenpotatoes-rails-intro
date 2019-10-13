27377b4a18001636cdb639d458bf07d5b49dea2e
“Autumn <qiushili@berkeley.edu>
2019-10-13 16:25:06 -0700

    checkbox

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ActiveRecord::Base
 
-    @@all_ratings = self.find(:first, :group => "rating").map(&:rating)
+    @@all_ratings = self.find(:all, :group => "rating").map(&:rating)
     def get_all_ratings
         @@all_ratings
     end
