5a713723b9a23f7d72f0a7c56d4967c2e931681a
“Autumn <qiushili@berkeley.edu>
2019-10-13 20:50:35 -0700

    checkbox

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,7 +13,7 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
     @all_ratings = Movie.get_all_ratings
-    @ratings = params[:rating]
+    @ratings = session[:rating].keys()
     @checked_ratings = @all_ratings
 
     
@@ -26,7 +26,7 @@ class MoviesController < ApplicationController
     end
 
     if params[:sort_by].nil? && params[:ratings].nil? && session[:ratings] && session[:sort_by]
-      @ratings = session[:rating]
+      @ratings = session[:rating].keys()
       @sorting = session[:sort_by]
       flash.keep
       redirect_to movies_path(:sort_by => @sorting, :ratings => @ratings)
